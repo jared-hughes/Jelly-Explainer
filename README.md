5af5f5f35a5d2e4a96eabd7f33f7bdf0130365ef
Jared Hughes <jahughes241@gmail.com>
2020-06-18 21:27:10 -0700

    Update to Python 3 and for latest Jelly compatability

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Jelly-Explainer
-Aims to make an automated explainer for programs in Dennis Mitchell's esoteric language Jelly.
+Aims to make an automated low-level explainer for programs in Dennis Mitchell's esolang Jelly.
 
 This is VERY MUCH a work in progress right now
 
 - - - - -
 
-# SETUP
+## Setup
 
-  1. Run setup.sh as root
-      sudo bash setup.sh
+  1. Run setup.sh
+
+  ```
+  ./setup.sh
+  ```
+
   2. Done setting up!
 
-# USAGE
+## Usage
 
 There is not a proper interface yet, but you can use the program by running named_parsing.py with python3. Run the code `explain(test_string)`, where `test_string` is the program you want to explain.
-
-# Troubleshooting
-
-If you an error which states `encoding not declared`, add the line `#-*- encoding:utf-8 -*-` to the top of all files in question.
--- setup.sh
+++ setup.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root (with sudo or su)"
-  exit
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -r jelly.wiki &> /dev/null
-rm -r jelly_prog &> /dev/null
+rm -rf jelly.wiki &> /dev/null
+rm -rf jelly_prog &> /dev/null
 
 out=0
 git clone https://github.com/DennisMitchell/jelly.git "$DIR/jelly_prog"
 EXITCODE=$?
 if [ $EXITCODE -eq 0 ]; then
-  echo "Clone of wiki successful"
+  echo "Clone of Jelly successful"
 else
-  echo "Clone of wiki failed with exit code " $EXITCODE
+  echo "Clone of Jelly failed with exit code " $EXITCODE
   out=5
 fi
 git clone https://github.com/DennisMitchell/jelly.wiki.git "$DIR/jelly.wiki"
 EXITCODE=$?
 if [ $EXITCODE -eq 0 ]; then
-  echo "Clone of wiki successful"
+  echo "Clone of Jelly wiki successful"
 else
-  echo "Clone of wiki failed with exit code " $EXITCODE
+  echo "Clone of Jelly wiki failed with exit code " $EXITCODE
   out=7
 fi
-mv "$DIR/jelly_prog/jelly.py" "$DIR/jelly.py"
-mv "$DIR/jelly_prog/dictionary.py" "$DIR/dictionary.py"
 
 python3 token_descriptions.py
 
